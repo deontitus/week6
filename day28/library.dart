e186d3c515fe58346a09e939fb6d9d3c2ede3b22
deontitus <deontitus4558@gmail.com>
2026-05-07 15:00:52 +0530

    Commpleted Day 28

---
+++ day28/library.dart
@@ -0,0 +1,35 @@
+// Arrow Function + Optional Named Parameter + where()
+
+List<String> searchBooks(
+  List<Map<String, String>> books, {
+  String? category,
+}) =>
+    books
+        .where((book) =>
+            category == null || book['category'] == category)
+        .map((book) => book['title']!)
+        .toList();
+
+void main() {
+  List<Map<String, String>> books = [
+    {"title": "Harry Potter", "category": "Fantasy"},
+    {"title": "The Hobbit", "category": "Fantasy"},
+    {"title": "Atomic Habits", "category": "Self Help"},
+    {"title": "Rich Dad Poor Dad", "category": "Finance"},
+  ];
+
+  // Search Fantasy books
+  var fantasyBooks = searchBooks(
+    books,
+    category: "Fantasy",
+  );
+
+  print("Fantasy Books:");
+  print(fantasyBooks);
+
+  // Search all books
+  var allBooks = searchBooks(books);
+
+  print("\nAll Books:");
+  print(allBooks);
+}
